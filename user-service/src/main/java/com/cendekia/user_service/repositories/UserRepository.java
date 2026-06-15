716b079ee5b693c68dcbafda17f55628645d45ec
azureeeeeeeeeeee <rionandosoeksin@gmail.com>
2026-06-15 10:10:46 +0800

    feat: init user repository

---
+++ user-service/src/main/java/com/cendekia/user_service/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.cendekia.user_service.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cendekia.user_service.enums.Role;
+import com.cendekia.user_service.models.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUid(String uid);
+    List<User> findByRole(Role role);
+}
