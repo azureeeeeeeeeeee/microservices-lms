518420f94a3ba53faea8d6c046f72e5ed37ff1f9
azureeeeeeeeeeee <rionandosoeksin@gmail.com>
2026-07-01 13:17:41 +0800

    feat: add validation to request dto

--- user-service/src/main/java/com/cendekia/user_service/dtos/RegisterRequestDTO.java
+++ user-service/src/main/java/com/cendekia/user_service/dtos/RegisterRequestDTO.java
@@ -2,6 +2,7 @@ package com.cendekia.user_service.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,17 +11,26 @@ import lombok.Setter;
 @Setter
 public class RegisterRequestDTO {
     @Size(min = 8, max = 255, message = "UID must be at least 8 characters long")
+    @NotBlank(message = "UID is required")
+    @NotNull(message = "UID is required")
     private String uid;
 
     @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
+    @NotNull(message = "Email is required")
     private String email;
 
     @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters long")
+    @NotBlank(message = "Password is required")
+    @NotNull(message = "Password is required")
     private String password;
 
     @NotBlank(message = "Full name is required")
+    @NotNull(message = "Full name is required")
     private String fullname;
 
     @Size(min = 8, max = 100, message = "Confirm password must be between 8 and 100 characters long")
+    @NotBlank(message = "Confirm password is required")
+    @NotNull(message = "Confirm password is required")
     private String confirmPassword;
 }
