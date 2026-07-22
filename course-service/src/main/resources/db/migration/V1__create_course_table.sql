aa1b9034df1660ba4810c7ac0ae4f6a5ec28c501
azureeeeeeeeeeee <rionandosoeksin@gmail.com>
2026-07-22 14:09:16 +0800

    feat: init sql migration

---
+++ course-service/src/main/resources/db/migration/V1__create_course_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE courses (
+    id UUID PRIMARY KEY,
+
+    title VARCHAR(255) UNIQUE NOT NULL,
+    description TEXT NOT NULL,
+
+    instructor_id UUID NOT NULL,
+    created_by UUID NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+
+)
