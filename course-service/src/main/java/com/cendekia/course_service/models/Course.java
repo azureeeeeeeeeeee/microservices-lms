bc4158d710a331256f0120db1cb3ffce950049bc
azureeeeeeeeeeee <rionandosoeksin@gmail.com>
2026-07-23 11:58:35 +0800

    feat: init model entity and docker compose

---
+++ course-service/src/main/java/com/cendekia/course_service/models/Course.java
@@ -0,0 +1,47 @@
+package com.cendekia.course_service.models;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "courses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "title", nullable = false)
+    @Size(min = 8, max = 255)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    @Size(min = 30, max = 2000)
+    private String description;
+
+    @Column(name = "instructor_id", nullable = false)
+    private UUID instructor_id;
+
+    @Column(name = "craeted_by", nullable = false)
+    private UUID created_by;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+}
