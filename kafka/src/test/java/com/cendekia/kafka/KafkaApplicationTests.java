c1b03a675b19b4edd53daff533f84672caa1c6a3
azureeeeeeeeeeee <rionandosoeksin@gmail.com>
2026-09-16 00:14:09 +0800

    feat: init kafka

---
+++ kafka/src/test/java/com/cendekia/kafka/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cendekia.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
